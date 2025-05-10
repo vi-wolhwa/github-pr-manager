603f3eede5b6a7e9c947dde06c03ea6e994bc006
stat1202 <hseungjae1202@naver.com>
2025-05-10 20:53:31 +0900

    feat: 옵션 페이지 삭제

--- manifest.js
+++ manifest.js
@@ -20,7 +20,6 @@ const manifest = {
   side_panel: {
     default_path: 'src/pages/sidepanel/index.html',
   },
-  options_page: 'src/pages/options/index.html',
   background: {
     service_worker: 'src/pages/background/index.js',
     type: 'module',

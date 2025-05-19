66c1479b0635ecc5bea5a00cdba0f1ace8b217fc
vi-wolhwa <cerezo00@naver.com>
2025-05-20 04:05:35 +0900

    feat: [Preview] 필요한 Selector 값 추가

---
+++ src/pages/content/pr-editor-preview/constants/selector.ts
@@ -0,0 +1,14 @@
+const SELECTOR = {
+  COMPARE: {
+    /** PR Editor의 NavBar와 Content를 포함하는 요소 */
+    editor: '.js-previewable-comment-form',
+    /** Navbar의 View(write, preview) 탭 목록을 포함하는 요소 */
+    editorNavbarViewTypeTabs: '.tabnav-tabs',
+    /** Content의 write 영역을 포함하는 요소 */
+    editorContentWrite: '.js-upload-markdown-image',
+    /** Content의 preview 영역을 포함하는 요소 */
+    editorContentPreview: '.js-preview-panel',
+  },
+};
+
+export default SELECTOR;

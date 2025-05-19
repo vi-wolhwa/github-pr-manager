a10711210aeaa714ff4e7b7eac7ecb23b44e6b31
vi-wolhwa <cerezo00@naver.com>
2025-05-20 03:40:49 +0900

    feat: [Preview] 미사용 상수 제거 (PAGE_URL)

--- src/pages/content/shared/constants/pageUrls.ts
+++ src/pages/content/shared/constants/pageUrls.ts
@@ -1,11 +1,6 @@
 /** 페이지 명칭 (ex. 'github.com/.../.../compare' -> 'compare') */
 export type PageName = 'naver' | 'compare';
 
-/** 페이지 URL */
-export const PAGE_URL: Record<PageName, string> = {
-  naver: 'naver.com',
-};
-
 /** 페이지 URL 정규식 */
 export const PAGE_URL_REGEX: Record<PageName, RegExp> = {
   naver: /naver\.com/,

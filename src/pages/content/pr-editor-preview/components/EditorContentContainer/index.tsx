81305380557bcc232fe0cdcb7037a98665c55c3f
vi-wolhwa <cerezo00@naver.com>
2025-05-20 04:06:46 +0900

    feat: [Preview] (미완성) EditorContentContainer 컴포넌트 구현

---
+++ src/pages/content/pr-editor-preview/components/EditorContentContainer/index.tsx
@@ -0,0 +1,7 @@
+import COMPONENT_ID from '../../constants/componentId';
+
+const EditorContentContainer = () => {
+  return <div id={COMPONENT_ID.EditorContentContainer}></div>;
+};
+
+export default EditorContentContainer;

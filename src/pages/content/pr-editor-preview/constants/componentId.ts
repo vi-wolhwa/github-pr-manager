a0232b0f397ef365a6f4c34e9c9988bf4f58ccf5
vi-wolhwa <cerezo00@naver.com>
2025-05-20 04:06:04 +0900

    feat: [Preview] 필요한 컴포넌트 Id를 상수로 선언

---
+++ src/pages/content/pr-editor-preview/constants/componentId.ts
@@ -0,0 +1,7 @@
+import COMPONENT_ID_PREFIX from '../../shared/constants/componentIdPrefix';
+
+const COMPONENT_ID = {
+  EditorContentContainer: `${COMPONENT_ID_PREFIX}-editor-content-container`,
+};
+
+export default COMPONENT_ID;

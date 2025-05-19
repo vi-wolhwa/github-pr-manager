1a6b669320e745caf7548e7bf62a4db3a34f6d83
vi-wolhwa <cerezo00@naver.com>
2025-05-20 04:09:58 +0900

    feat: [Preview] (미완성) runPREditorPreviewScript 구현

---
+++ src/pages/content/pr-editor-preview/index.tsx
@@ -0,0 +1,56 @@
+import updateDom from '../shared/utils/updateDom';
+import { isCurrentPage } from '../shared/utils/siteUtils';
+import SELECTOR from './constants/selector';
+import EditorNavbarSplitTabButton from './components/EditorNavbarSplitTabButton';
+import getDomElement from '../shared/utils/getDomElement';
+import EditorContentContainer from './components/EditorContentContainer/index';
+import COMPONENT_ID from './constants/componentId';
+
+/**
+ * PR Editor Preview 기능 스크립트
+ */
+const runPREditorPreviewScript = async () => {
+  if (isCurrentPage('compare')) {
+    /* Editor > Navbar > View 탭에 'split' 버튼 렌더링 */
+    updateDom({
+      action: 'append',
+      targetSelector: SELECTOR.COMPARE.editorNavbarViewTypeTabs,
+      component: <EditorNavbarSplitTabButton />,
+    });
+
+    /** Editor > Content > Write 영역 */
+    const editorContentWrite = await getDomElement({
+      action: 'cut',
+      targetSelector: SELECTOR.COMPARE.editorContentWrite,
+    });
+
+    /** Editor > Content > Preview 영역 */
+    const editorContentPreview = await getDomElement({
+      action: 'cut',
+      targetSelector: SELECTOR.COMPARE.editorContentPreview,
+    });
+
+    console.log(editorContentPreview);
+
+    /* Editor > Content 내부 요소(write, preview, split)를 포함하는 컨테이너 렌더링 */
+    updateDom({
+      action: 'append',
+      targetSelector: SELECTOR.COMPARE.editor,
+      component: <EditorContentContainer />,
+    });
+
+    updateDom({
+      action: 'append',
+      targetSelector: `#${COMPONENT_ID.EditorContentContainer}`,
+      htmlElement: editorContentWrite,
+    });
+
+    updateDom({
+      action: 'append',
+      targetSelector: `#${COMPONENT_ID.EditorContentContainer}`,
+      htmlElement: editorContentPreview,
+    });
+  }
+};
+
+export default runPREditorPreviewScript;

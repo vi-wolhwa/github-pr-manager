e89634aedd229fbcad73b1bce3d4beb0a8f4f570
stat1202 <hseungjae1202@naver.com>
2025-05-25 13:50:44 +0900

    feat: setInterval 추가

--- src/pages/content/prTitleAutoInsert/index.tsx
+++ src/pages/content/prTitleAutoInsert/index.tsx
@@ -7,10 +7,23 @@ import { insertPRTitle } from './helpers/insertPRTitle';
  * 테스트를 위한 Sample Content 스크립트
  */
 const runPRTitleAutoInsert = async () => {
-  if (isCurrentPage('github')) {
-    const prBranches = getBranchName();
-    const prTitle = await getPRTitle(prBranches);
-    insertPRTitle(prTitle);
+  let prevBranchName = {
+    base: '',
+    compare: '',
+  };
+
+  if (isCurrentPage('github_pr')) {
+    setInterval(async () => {
+      const prBranches = getBranchName();
+      if (prevBranchName.base === prBranches.base && prevBranchName.compare === prBranches.compare) {
+        return; // 브랜치 이름이 변경되지 않았다면 함수 종료
+      }
+      prevBranchName = { ...prBranches };
+
+      const prTitle = await getPRTitle(prBranches);
+
+      insertPRTitle(prTitle);
+    }, 500);
   }
 };
 
--- src/pages/content/shared/constants/pageUrls.ts
+++ src/pages/content/shared/constants/pageUrls.ts
@@ -1,14 +1,16 @@
 /** 페이지 명칭 */
-export type PageName = 'naver' | 'github';
+export type PageName = 'naver' | 'github' | 'github_pr';
 
 /** 페이지 URL */
 export const PAGE_URL: Record<PageName, string> = {
   naver: 'naver.com',
   github: 'github.com',
+  github_pr: 'github.com',
 };
 
 /** 페이지 URL 정규식 */
 export const PAGE_URL_REGEX: Record<PageName, RegExp> = {
   naver: /naver\.com/,
   github: /github\.com/,
+  github_pr: /^https:\/\/github\.com\/[^/]+\/[^/]+\/compare\/[^?]+\.{3}[^?]+(?:\?.*)?$/,
 };

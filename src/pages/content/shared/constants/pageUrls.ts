8b4bbe9dfb5a252071e8274373416df5a46cacda
이보현 <92661884+frombozztoang@users.noreply.github.com>
2025-06-07 14:27:06 +0900

    fix: pr template 생성 페이지 정규식 조금 더 유연하게 조건 변경

--- src/pages/content/shared/constants/pageUrls.ts
+++ src/pages/content/shared/constants/pageUrls.ts
@@ -12,6 +12,12 @@ export const PAGE_URL: Record<PageName, string> = {
 export const PAGE_URL_REGEX: Record<PageName, RegExp> = {
   naver: /naver\.com/,
   github: /github\.com/,
-  /** github_pr_create ex) /owner/repo/compare/feat/test, /owner/repo/compare/feat/test?expand=1 */
-  github_pr_create: /^\/[^/]+\/[^/]+\/compare(\/[^?]+)?(\?.*)?$/,
+  /**
+   * github_pr_create
+   * ex) /owner/repo/compare/feat/test, /owner/repo/compare/fix/bug?expand=1
+   *
+   * - 브랜치 이름에는 /, -, _, ~, +, ^ 등 다양한 문자 포함 가능
+   * - ?expand=1 같은 쿼리스트링도 허용
+   */
+  github_pr_create: /^\/[^/]+\/[^/]+\/compare\/[^?]+(\?.*)?$/,
 };

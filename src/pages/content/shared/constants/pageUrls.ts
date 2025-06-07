6de621c7421e298240f7973bcdf4f7e32d5c8ff6
vi-wolhwa <cerezo00@naver.com>
2025-06-07 18:47:01 +0900

    fix: pageUrl 정규식 수정

--- src/pages/content/shared/constants/pageUrls.ts
+++ src/pages/content/shared/constants/pageUrls.ts
@@ -4,5 +4,5 @@ export type PageName = 'naver' | 'pulls';
 /** 페이지 URL 정규식 */
 export const PAGE_URL_REGEX: Record<PageName, RegExp> = {
   naver: /naver\.com/,
-  pulls: /github\.com\/[^/]+\/[^/]+\/pulls\//,
+  pulls: /github\.com\/[^/]+\/[^/]+\/pulls\/?/,
 };

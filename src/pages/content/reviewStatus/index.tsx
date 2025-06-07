b08bce5fb0d47031dd69a460a0c9bed32b43a2b8
vi-wolhwa <cerezo00@naver.com>
2025-06-07 18:32:40 +0900

    feat: pulls url 스크립트 기반 작업

---
+++ src/pages/content/reviewStatus/index.tsx
@@ -0,0 +1,11 @@
+import { isCurrentPage } from '../shared/utils/siteUtils';
+
+/**
+ * PR 리뷰 상태 관리 기능 스크립트
+ */
+const runPREditorPreviewScript = async () => {
+  if (isCurrentPage('pulls')) {
+  }
+};
+
+export default runPREditorPreviewScript;
--- src/pages/content/shared/constants/pageUrls.ts
+++ src/pages/content/shared/constants/pageUrls.ts
@@ -1,12 +1,8 @@
 /** 페이지 명칭 */
-export type PageName = 'naver';
-
-/** 페이지 URL */
-export const PAGE_URL: Record<PageName, string> = {
-  naver: 'naver.com',
-};
+export type PageName = 'naver' | 'pulls';
 
 /** 페이지 URL 정규식 */
 export const PAGE_URL_REGEX: Record<PageName, RegExp> = {
   naver: /naver\.com/,
+  pulls: /github\.com\/[^/]+\/[^/]+\/pulls\//,
 };
